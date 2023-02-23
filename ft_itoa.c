6a671732a435510c2b12e321a362361aa9dbb389
Yamennes <125287946+Yamennes@users.noreply.github.com>
2023-02-23 13:32:41 +0100

    Add files via upload

--- ft_itoa.c
+++ ft_itoa.c
@@ -3,53 +3,54 @@
 /*                                                        :::      ::::::::   */
 /*   ft_itoa.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lduthill <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: roroca <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/21 11:03:06 by lduthill          #+#    #+#             */
-/*   Updated: 2023/02/21 11:03:08 by lduthill         ###   ########.fr       */
+/*   Created: 2023/02/09 13:33:36 by roroca            #+#    #+#             */
+/*   Updated: 2023/02/22 16:28:35 by roroca           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strrev(char *str, int a, int n, int num)
+static char	*ft_strrev(char *str, int neg, int n, int len)
 {
-	while (num >= 0)
+	str[len] = '\0';
+	len--;
+	while (len >= 0)
 	{
-		str[num] = (a * (n % 10)) + 48;
+		str[len] = (neg * (n % 10)) + 48;
 		n /= 10;
-		num--;
+		len--;
 	}
-	if (a == -1)
+	if (neg == -1)
 		str[0] = '-';
 	return (str);
 }
 
 char	*ft_itoa(int n)
 {
-	char	*str;
-	int		num;
-	int		a;
+	int		nbr;
+	int		neg;
 	int		len;
+	char	*str;
 
-	a = 1;
-	len = 1;
+	nbr = n;
+	neg = 1;
+	len = 0;
 	if (n < 0)
 	{
-		len = 2;
-		a = -1;
+		len++;
+		neg = -1;
 	}
-	num = n;
-	while (num / 10 != 0)
+	while (nbr >= 10 || nbr <= -10)
 	{
-		num /= 10;
+		nbr /= 10;
 		len++;
 	}
+	len++;
 	str = malloc(sizeof(char) * (len + 1));
 	if (!str)
 		return (NULL);
-	str[len] = '\0';
-	num = len - 1;
-	str = ft_strrev(str, a, n, num);
+	str = ft_strrev(str, neg, n, len);
 	return (str);
 }
